81421b74c6714d4b1724bb7838047998ee3f1630
naoki fujiwara <nfujiwara@hotmail.com>
2021-02-26 22:13:22 -0800

    js file revised closing the modal by clicking outside of the modal

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 const modalBtn = document.querySelector('.jsModalBtn')
 const modalBtnClose = document.querySelector('.jsModalClose')
 const bod = document.body
+const modalOverlay = document.querySelector('.modal-overlay')
 
-modalBtn.addEventListener('click', ()=> bod.classList.add('modal-is-open'))
+modalBtn.addEventListener('click', ()=> 
+bod.classList.add('modal-is-open'))
 
 modalBtnClose.addEventListener('click', ()=>bod.classList.remove('modal-is-open'))
+
+modalOverlay.addEventListener('click', function(e){
+  if(!e.target.closest('.modal')){
+    bod.classList.remove('modal-is-open')
+    console.log(e.target)
+  }
+
+})
+
