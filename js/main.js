01b96429ecf176f04251538414d819d5b3f5e7e9
naoki fujiwara <nfujiwara@hotmail.com>
2021-02-27 21:23:11 -0800

    rev js evernt delegation

--- js/main.js
+++ js/main.js
@@ -1,18 +1,40 @@
+// const modalBtn = document.querySelector('.jsModalBtn')
+// const modalBtnClose = document.querySelector('.jsModalClose')
+// const bod = document.body
+// const modalOverlay = document.querySelector('.modal-overlay')
+
+// modalBtn.addEventListener('click', ()=> 
+// bod.classList.add('modal-is-open'))
+
+// modalBtnClose.addEventListener('click', ()=>bod.classList.remove('modal-is-open'))
+
+// modalOverlay.addEventListener('click', function(e){
+//   if(!e.target.closest('.modal')){
+//     bod.classList.remove('modal-is-open')
+//     console.log(e.target)
+//     console.log(e.currentTarget)
+//   }
+//   // bod.classList.remove('modal-is-open')
+//   // console.log(e.target)
+//   // console.log(e.currentTarget)
+
+// })
+
 const modalBtn = document.querySelector('.jsModalBtn')
-const modalBtnClose = document.querySelector('.jsModalClose')
+const modalCloseBtn = document.querySelector('.jsModalClose')
 const bod = document.body
-const modalOverlay = document.querySelector('.modal-overlay')
+const modalOverlay = document.querySelector('.jsModalOverLay')
 
-modalBtn.addEventListener('click', ()=> 
-bod.classList.add('modal-is-open'))
-
-modalBtnClose.addEventListener('click', ()=>bod.classList.remove('modal-is-open'))
-
-modalOverlay.addEventListener('click', function(e){
-  if(!e.target.closest('.modal')){
-    bod.classList.remove('modal-is-open')
-    console.log(e.target)
-  }
-
+modalBtn.addEventListener('click', function(e){
+  bod.classList.add('modal-is-open')
 })
 
+modalCloseBtn.addEventListener('click', function(e){
+  bod.classList.remove('modal-is-open')
+})
+
+modalOverlay.addEventListener('click', function(e){
+    if(!e.target.closest('.modal')){
+      bod.classList.remove('modal-is-open')
+    }
+})
